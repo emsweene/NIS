80512b93b5c965b326e980f03588b8bf1b04df4c
Elizabeth Sweeney <emsweene@gmail.com>
2013-09-02 10:16:59 -0400

    Update_20130901

--- NIS_Analysis.R
+++ NIS_Analysis.R
@@ -10,6 +10,7 @@
 ###############################################################################################
 
 
+
 ###############################################################################################
 ## Load patient records from all years 
 ###############################################################################################
@@ -37,9 +38,9 @@ data <- rbind(data_2006, data_2007, data_2008, data_2009, data_2010)
 dim(data)
 ## 944016     24
 
-data_over_16 <- data[as.numeric(data$AGE) > 16,]
+data_over_16 <- data[as.numeric(as.charachter(data$AGE)) > 16,]
 dim(data_over_16)
-## 941087     24
+## 940773     24
 
 ###############################################################################################
 ## Information for table 1
@@ -53,11 +54,17 @@ ais_totals <- cbind(dim(data_over_16[data_over_16$Year == 2006,])[1],
 
 print(ais_totals)
 
-##[,1]   [,2]   [,3]   [,4]   [,5]
-##[1,] 185874 181393 196398 187183 190239
+##       [,1]   [,2]   [,3]   [,4]   [,5]
+##[1,] 185865 181322 196360 187146 190080
 
 sum(ais_totals)
-## 941087
+## 940773
+
+round(ais_totals/sum(ais_totals) *100, 1)
+
+##     [,1] [,2] [,3] [,4] [,5]
+## [1,] 19.8 19.3 20.9 19.9 20.2
+
 
 ##AIS and HIV + Patients (by year)
 
@@ -65,11 +72,13 @@ hiv_ais_totals <- cbind(sum(data_over_16$hiv[data_over_16$Year == 2006]),
   sum(data_over_16$hiv[data_over_16$Year == 2007]), sum(data_over_16$hiv[data_over_16$Year == 2008]), 
   sum(data_over_16$hiv[data_over_16$Year == 2009]), sum(data_over_16$hiv[data_over_16$Year == 2010]))
 
-## [,1] [,2] [,3] [,4] [,5]
-##[1,]  556  519  641  568  598
+     [,1] [,2] [,3] [,4] [,5]
+[1,]  555  519  641  567  595
 
 sum(hiv_ais_totals)
-[1] 2882
+[1] 2877
+
+round(hiv_ais_totals/sum(hiv_ais_totals) *100, 1)
 
 ## AIS, HIV -  and tPA
 
@@ -81,13 +90,18 @@ no_hiv_ais_tpa_totals <- cbind(dim(data_over_16[data_over_16$hiv == 0 & data_ove
   data_over_16$Year == 2010,])[1])
 
 print(no_hiv_ais_tpa_totals)
-##[,1] [,2] [,3] [,4] [,5]
-##[1,] 2702 2975 3714 4400 5549
+##     [,1] [,2] [,3] [,4] [,5]
+##[1,] 2700 2974 3714 4399 5548
 
 sum(no_hiv_ais_tpa_totals)
-##19340
+##19335
 
-## AIS, HIV -  and tPA
+round(no_hiv_ais_tpa_totals/sum(no_hiv_ais_tpa_totals) *100, 1)
+
+##      [,1] [,2] [,3] [,4] [,5]
+## [1,]   14 15.4 19.2 22.8 28.7
+
+## AIS, HIV + and tPA
 
 hiv_ais_tpa_totals <- cbind(dim(data_over_16[data_over_16$hiv == 1 & data_over_16$tpa == 1 & 
   data_over_16$Year == 2006,])[1], dim(data_over_16[data_over_16$hiv == 1 & data_over_16$tpa == 1 & 
@@ -102,22 +116,165 @@ print(hiv_ais_tpa_totals)
 ##[1,]    6    6   10   16   25
 
 sum(hiv_ais_tpa_totals)
-## [1] 63
+## [1] 62
 
+
+round(hiv_ais_tpa_totals/sum(hiv_ais_tpa_totals) *100, 1)
+##      [,1] [,2] [,3] [,4] [,5]
+## [1,]  9.7  9.7 16.1 24.2 40.3
+
+
+##total HIV - with ais
+sum(ais_totals) - sum(hiv_ais_totals)
+
+###############################################################################################
+## Test for difference in IV_TPA use in the two groups (AIS HIV+ and AIS HIV-)
+###############################################################################################
+x <- matrix(c(sum(hiv_ais_tpa_totals), sum(hiv_ais_totals) , sum(no_hiv_ais_tpa_totals), 
+sum(ais_totals) - sum(hiv_ais_totals)), 2,2)
+chisq.test(x)
+##p-value = 0.78
+
+###############################################################################################
+###############################################################################################
+## Calculate the demographics for the two groups that recieved tPA  (AIS HIV+ TPA and AIS 
+## HIV- TPA)
+###############################################################################################
+###############################################################################################
+
+###############################################################################################
+##HIV+ and TPA
+###############################################################################################
+
+median(as.numeric(as.character(data_over_16[data_over_16$hiv == 1 & data_over_16$tpa == 1,]$AGE)))
+##52
+
+range(as.numeric(as.character(data_over_16[data_over_16$hiv == 1 & data_over_16$tpa == 1,]$AGE)))
+## [1] 27 78
+
+table(as.numeric(as.character(data_over_16[data_over_16$hiv == 1 & data_over_16$tpa == 1,]$FEMALE)))
+## 0  1
+## 42 20
+
+20/62
+## 0.3225806
+
+table(as.numeric(as.character(data_over_16[data_over_16$hiv == 1 & data_over_16$tpa == 1,]$RACE)))
+
+## -9  1  2  3  5
+##  7 12 35  6  2
+
+12/(35 + 12 + 6 + 2)
+## 0.2181818
+
+###############################################################################################
+##HIV- and TPA
+###############################################################################################
+
+median(as.numeric(as.character(data_over_16[data_over_16$hiv == 0 & data_over_16$tpa == 1,]$AGE)))
+##72
+
+range(as.numeric(as.character(data_over_16[data_over_16$hiv == 0 & data_over_16$tpa == 1,]$AGE)))
+## [1] 17 102
+
+table(as.numeric(as.character(data_over_16[data_over_16$hiv == 0 & data_over_16$tpa == 1,]$FEMALE)))
+##  -9    0    1
+##   2 9621 9712 
+
+9712/(9621 + 9712)
+##  0.5023535
+
+table(as.numeric(as.character(data_over_16[data_over_16$hiv == 0 & data_over_16$tpa == 1,]$RACE)))
+
+##    -9    -8     1     2     3     4     5     6 
+##  3458     1 11821  2141  1013   406    72   423 
+
+11821/(11821 + 2141 + 1013 + 406 + 72 + 423)
+## 0.744583
+
+###############################################################################################
+## The regression
+###############################################################################################
 ###############################################################################################
 ## The Outcome in our regression is death; dicharge to hospice is considered death, DISPUB92 
 ## code of 50 and 51
 ###############################################################################################
 
 Outcome <- as.numeric(as.character(data_over_16$DIED))
+table(Outcome)
+
+##Outcome
+##    -9     -8      0      1
+##   205    144 891115  49309
 
 sum(as.numeric(as.character(data_over_16$DISPUB92))== 50)
-## 7170
+## 7213
 sum(as.numeric(as.character(data_over_16$DISPUB92))== 51)
-## 14305
+## 14391
+
+table(Outcome[data_over_16$hiv == 0])
+
+##     -9     -8      0      1 
+##    205    144 888458  49089
+
+table(Outcome[data_over_16$hiv == 1])
+##   0    1
+## 2657  220
+
+x <- matrix(c(220, 2877, 49089, 937547), 2, 2) 
+chisq.test(x)
+
+##for the group with TPA##
+
+table(Outcome[data_over_16$hiv == 0 &  data_over_16$tpa == 1])
+
+##        -9    -8     0     1 
+##        8     5  17221  2101 
+
+table(Outcome[data_over_16$hiv == 1 &  data_over_16$tpa == 1])
+##   0  1 
+##  55  7
+
+
+x <- matrix(c(7, 62, 2101 , 19322), 2, 2) 
+chisq.test(x)
+
+
+##add outcome of hosspice 
+
 
 Outcome[as.numeric(as.character(data_over_16$DISPUB92))== 50] <- 1
 Outcome[as.numeric(as.character(data_over_16$DISPUB92))== 51] <- 1
+
+table(Outcome[data_over_16$hiv == 0])
+
+##    -9     -8      0      1
+##   205    144 866925  70622 
+
+table(Outcome[data_over_16$hiv == 1])
+##   0    1
+## 2586  291 
+
+x <- matrix(c(291, 2586 + 291, 70622, 70622 + 866925),2,2)
+chisq.test(x)
+
+
+##for the group with TPA##
+
+table(Outcome[data_over_16$hiv == 0 &  data_over_16$tpa == 1])
+
+##      -9    -8     0     1 
+##    8     5 16458  2864 
+
+table(Outcome[data_over_16$hiv == 1 &  data_over_16$tpa == 1])
+##   0  1 
+##  52 10 
+
+x <- matrix(c(10, 62, 2864 , 2864 + 16458),2,2)
+chisq.test(x)
+
+
+
 
 ###############################################################################################
 ## First, regression without adjusting for RACE 
@@ -125,14 +282,19 @@ Outcome[as.numeric(as.character(data_over_16$DISPUB92))== 51] <- 1
 regression_data <- data.frame(Outcome, RACE = as.numeric(as.character(data_over_16$RACE)), 
   FEMALE = as.numeric(as.character(data_over_16$FEMALE)), AGE = as.numeric(as.character(data_over_16$AGE)), 
   hiv = as.numeric(as.character(data_over_16$hiv)), tpa = as.numeric(as.character(data_over_16$tpa)))
+dim(regression_data)
+## 940773      6
+
+sum(regression_data$hiv)
+## [1] 2877
 
 ##Missing data
 sum(regression_data$FEMALE < 0)
-## 239
+##  179
 sum(regression_data$Outcome < 0)
-## 407
+## 349
 sum(regression_data$RACE < 0)
-## 184148
+## 184029
 
 ###############################################################################################
 ## Missing gender is recorded as a -6, -8 or -9, Missing death is recorded as 
@@ -141,77 +303,41 @@ sum(regression_data$RACE < 0)
 regression_data <- regression_data[regression_data$FEMALE >=0,]
 regression_data <- regression_data[regression_data$Outcome >=0,]
 
+dim(regression_data)
+## [1] 940245      6
+
 fit <- glm(Outcome ~ AGE + hiv + tpa + hiv*tpa + FEMALE, data=regression_data, family=binomial())
 
-##summary(fit)
+summary(fit)
 
 ##Call:
-##  glm(formula = Outcome ~ AGE + hiv + tpa + hiv * tpa + FEMALE, 
-##      family = binomial(), data = regression_data)
+##glm(formula = Outcome ~ AGE + hiv + tpa + hiv * tpa + FEMALE,
+##    family = binomial(), data = regression_data)
 
-##Deviance Residuals: 
-##  Min       1Q   Median       3Q      Max  
-##-0.8768  -0.4346  -0.3753  -0.3079   4.0865  
+##Deviance Residuals:
+##    Min       1Q   Median       3Q      Max
+##-0.9271  -0.4364  -0.3748  -0.3043   3.0454
 
 ##Coefficients:
-##  Estimate Std. Error  z value Pr(>|z|)    
-##(Intercept) -5.1278597  0.0262936 -195.023   <2e-16 ***
-##  AGE          0.0339163  0.0003433   98.784   <2e-16 ***
-##  hiv          1.1037888  0.0638800   17.279   <2e-16 ***
-##  tpa          0.8760296  0.0209377   41.840   <2e-16 ***
-##  FEMALE       0.1362565  0.0080441   16.939   <2e-16 ***
-##  hiv:tpa     -0.3235492  0.3538926   -0.914    0.361    
-##---
-##  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1 
+##              Estimate Std. Error  z value Pr(>|z|)
+##(Intercept) -5.2260511  0.0264411 -197.649   <2e-16 ***
+##AGE          0.0352089  0.0003445  102.213   <2e-16 ***
+##hiv          1.1328536  0.0639216   17.723   <2e-16 ***
+##tpa          0.8780151  0.0209562   41.898   <2e-16 ***
+##FEMALE       0.1347427  0.0080436   16.752   <2e-16 ***
+##hiv:tpa     -0.3178793  0.3544404   -0.897     0.37
+---
+##Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1 
 
 ##(Dispersion parameter for binomial family taken to be 1)
 
-##Null deviance: 501876  on 940440  degrees of freedom
-##Residual deviance: 488609  on 940435  degrees of freedom
-##AIC: 488621
+##    Null deviance: 502874  on 940244  degrees of freedom
+##Residual deviance: 488844  on 940239  degrees of freedom
+##AIC: 488856
 
 ##Number of Fisher Scoring iterations: 5
 
 ###############################################################################################
-## Remove subjects missing race, and adjust for race
+## Unadjusted odds ratios
 ###############################################################################################
-
-regression_data <- regression_data[regression_data$RACE >=0,]
-
-fit_1 <- glm(Outcome ~ AGE + hiv + tpa + hiv*tpa + FEMALE + as.factor(RACE), 
-  data=regression_data, family=binomial())
-
-summary(fit_1)
-
-##Call:
-##  glm(formula = Outcome ~ AGE + hiv + tpa + hiv * tpa + FEMALE + 
-##        as.factor(RACE), family = binomial(), data = regression_data)
-
-##Deviance Residuals: 
-##  Min       1Q   Median       3Q      Max  
-##-0.9043  -0.4343  -0.3747  -0.3070   4.1061  
-
-##Coefficients:
-##  Estimate Std. Error  z value Pr(>|z|)    
-##(Intercept)      -5.1811875  0.0302949 -171.025  < 2e-16 ***
-##  AGE               0.0341381  0.0003897   87.610  < 2e-16 ***
-##  hiv               1.0349441  0.0685208   15.104  < 2e-16 ***
-##  tpa               0.8711786  0.0231715   37.597  < 2e-16 ***
-##  FEMALE            0.1309174  0.0089978   14.550  < 2e-16 ***
-##  as.factor(RACE)2  0.1506781  0.0141683   10.635  < 2e-16 ***
-##  as.factor(RACE)3  0.1356576  0.0184975    7.334 2.24e-13 ***
-##  as.factor(RACE)4  0.0685120  0.0304355    2.251   0.0244 *  
-##  as.factor(RACE)5  0.0346540  0.0591698    0.586   0.5581    
-##as.factor(RACE)6  0.1886743  0.0281702    6.698 2.12e-11 ***
-##  hiv:tpa          -0.4604526  0.3910606   -1.177   0.2390    
-##---
-##  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1 
-
-##(Dispersion parameter for binomial family taken to be 1)
-
-##Null deviance: 402130  on 756581  degrees of freedom
-##Residual deviance: 391673  on 756571  degrees of freedom
-##AIC: 391695
-
-##Number of Fisher Scoring iterations: 5
 
